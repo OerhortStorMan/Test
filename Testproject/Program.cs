962fb0e190c644a480598e3efaf0c63b5ef5d160
OerhortStorMan <l.winelid@gmail.com>
2020-08-26 10:11:38 +0200

    Update Program.cs

--- Testproject/Program.cs
+++ Testproject/Program.cs
@@ -6,7 +6,7 @@ namespace Testproject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
